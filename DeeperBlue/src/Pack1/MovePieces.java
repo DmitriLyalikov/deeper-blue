9285a6a779a5123550b71f7270b6fa4f7034108a
Joel-Mendez0 <113056408+Joel-Mendez0@users.noreply.github.com>
2023-03-01 21:11:47 -0500

    Update MovePieces.java

--- DeeperBlue/src/Pack1/MovePieces.java
+++ DeeperBlue/src/Pack1/MovePieces.java
@@ -1,10 +1,41 @@
 package Pack1;
 
-
-// This class will allow the dragging and dropping of pieces and center them on the square
-// Will not be checking for illegal moves
-// When pieces are placed on top of one another, the piece that was on the square will vanish
-public class MovePieces {
+public class MovePieces extends DrawBoard{
+	
+	public static int Get_Starting_Row() {
+		int row;
+		return 0;
+	}
+	public static int Get_Starting_Col() {
+		int col;
+		return 0;
+	}
+	public static int Get_Ending_Row() {
+		int row;
+		return 0;
+	}
+	public static int Get_Ending_Col() {
+		int col;
+		return 0;
+	}
+	
+	public static byte[][] MakeMove(byte[][] currentboardstate, int Starting_Row, int Starting_Col, int Ending_Row, int Ending_Col){
+		byte[][] updated_boardstate = currentboardstate; 		
+		
+		DrawBoard place = new DrawBoard();
+		
+		byte Piece = currentboardstate[Starting_Row][Starting_Col];
+		
+		updated_boardstate[Ending_Row][Ending_Col] = Piece;
+		
+		
+		updated_boardstate[Starting_Row][Starting_Col] = (byte) place.EMPTY;
+ 
+		
+		
+		
+		return updated_boardstate;
+	}
 	
 	
 
